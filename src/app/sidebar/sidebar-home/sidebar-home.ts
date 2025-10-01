664f5bfd7807026c3cacf0b33e3b42df4f84d3e9
Aashin Thomas <aashinthomas072@gmail.com>
2025-10-01 10:24:18 +0530

    Fix: Fixed routing from navbar. TODO: set accordions and fix icons

--- src/app/sidebar/sidebar-home/sidebar-home.ts
+++ src/app/sidebar/sidebar-home/sidebar-home.ts
@@ -9,16 +9,28 @@ import {
   Shield, 
   BarChart3, 
   Settings,
-  ChevronDown 
+  ChevronDown, 
+  Circle
 } from 'lucide-angular';
 import { SidebarSingleSection } from '../sidebar-single-section/sidebar-single-section';
+import { SidebarAccordionSection } from '../sidebar-accordion-section/sidebar-accordion-section';
+import { SidebarSubSection } from '../sidebar-sub-section/sidebar-sub-section';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-sidebar-home',
   imports: [
     SidebarHeader,
     LucideAngularModule,
-    SidebarSingleSection
+    SidebarSingleSection,
+    SidebarAccordionSection,
+    SidebarSubSection,
+    RouterLinkActive,
+    RouterLink,
+    RouterModule,
+    RouterOutlet,
   ],
   templateUrl: './sidebar-home.html',
   styleUrl: './sidebar-home.css'
@@ -32,4 +44,77 @@ export class SidebarHome {
   readonly BarChart3 = BarChart3;
   readonly Settings = Settings;
   readonly ChevronDown = ChevronDown;
+  
+
+  // navbarItems = [
+  //   {
+  //     label: 'Dashboard',
+  //     icon: 'lucide lucide-house w-4 h-4',
+  //     route: '/dashboard',
+  //     children: [],
+  //     expanded: false,
+  //   },
+  //   {
+  //     label: 'Projects',
+  //     icon: 'folder',
+  //     route: '',
+  //     expanded: false,
+  //     children: [
+  //       { label: 'All Projects', route: '/projects' },
+  //       { label: 'Create Project', route: '/projects/create' },
+  //       { label: 'Import from Jira', route: '/projects/importfromjira' },
+  //     ],
+  //   },
+  //   {
+  //     label: 'DU Management',
+  //     icon: 'lucide lucide-building w-4 h-4',
+  //     route: '',
+  //     expanded: false,
+  //     children: [
+  //       { label: 'All DUs', route: '/dus/all' },
+  //       { label: 'Create DU', route: '/dus/invite' },
+  //     ],
+  //   },
+  //   {
+  //     label: 'User Management',
+  //     icon: 'lucide lucide-user w-4 h-4',
+  //     route: '',
+  //     expanded: false,
+  //     children: [
+  //       { label: 'All Users', route: '/users' },
+  //       { label: 'Create User', route: '/users/create' },
+  //     ],
+  //   },
+  //   {
+  //     label: 'Roles and Permissions',
+  //     icon: 'lucide lucide-shield w-4 h-4',
+  //     route: '',
+  //     expanded: false,
+  //     children: [
+  //       { label: 'All Roles', route: '/roles' },
+  //       { label: 'Create Role', route: '/roles/create' },
+  //     ],
+  //   },
+  //   {
+  //     label: 'Reports',
+  //     icon: 'lucide lucide-bar-chart-3 w-4 h-4',
+  //     route: '/reports',
+  //     expanded: false,
+  //     children: [],
+  //   },
+  //   {
+  //     label: 'Settings',
+  //     icon: 'lucide lucide-settings w-4 h-4',
+  //     route: '',
+  //     expanded: false,
+  //     children: [
+  //       { label: 'System Settings', route: '/settings/system' },
+  //       { label: 'Import/Export', route: '/settings/import' },
+  //     ],
+  //   },
+  // ];
+ 
+  toggle(item: any) {
+    item.expanded = !item.expanded;
+  }
 }
--- src/app/sidebar/sidebar-single-section/sidebar-single-section.ts
+++ src/app/sidebar/sidebar-single-section/sidebar-single-section.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-sidebar-single-section',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './sidebar-single-section.html',
   styleUrl: './sidebar-single-section.css'
 })
 export class SidebarSingleSection {
   @Input() icon: string = '';
   @Input() label: string = '';
+  @Input() route: string = '';
 
   constructor() {}
-
-  
-
 }
