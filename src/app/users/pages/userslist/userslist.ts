11445de1b5c00b6721636d6c862e3d1568eac98a
Aashin Thomas <aashinthomas072@gmail.com>
2025-10-03 17:43:11 +0530

    Feat: Perfected the section Title font size and added button to user management page

--- src/app/users/pages/userslist/userslist.ts
+++ src/app/users/pages/userslist/userslist.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { Sectiontitle } from '../../../shared/sectiontitle/sectiontitle';
+import { CustomButton } from '../../../shared/custom-button/custom-button';
 
 @Component({
   selector: 'app-userslist',
-  imports: [Sectiontitle],
+  imports: [Sectiontitle, CustomButton],
   templateUrl: './userslist.html',
   styleUrl: './userslist.css'
 })
 export class Userslist {
+  onAddUser() {
+    // Logic to add a new user
+    console.log('Add User button clicked');
+  }
 
 }
