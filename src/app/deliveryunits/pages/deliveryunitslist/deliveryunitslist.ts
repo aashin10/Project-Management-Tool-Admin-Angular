29805f494dd8e0ed97e557773dfd0606eb154f59
aswinrajpv <aswinraj1633@gmail.com>
2025-10-03 09:58:06 +0530

    made basic structure for the reusable table

--- src/app/deliveryunits/pages/deliveryunitslist/deliveryunitslist.ts
+++ src/app/deliveryunits/pages/deliveryunitslist/deliveryunitslist.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Table } from "../../../shared/table/table";
 
 @Component({
   selector: 'app-deliveryunitslist',
-  imports: [],
+  imports: [Table],
   templateUrl: './deliveryunitslist.html',
   styleUrl: './deliveryunitslist.css'
 })
 export class Deliveryunitslist {
+// In your user-management component
+columns = [
+  { header: 'User', field: 'user', type: 'user' as const },
+  { header: 'Type', field: 'type', type: 'badge' as const },
+  { header: 'Status', field: 'status', type: 'badge' as const },
+  { header: 'Created', field: 'created' },
+  { header: 'Last Activity', field: 'lastActivity' },
+  { header: 'Actions', field: 'actions', type: 'actions' as const }
+];
 
+users = [
+  {
+    user: { avatar: 'JA', name: 'John Admin', email: 'john.admin@company.com' },
+    type: 'internal',
+    status: 'active',
+    created: '2024-01-01',
+    lastActivity: '2025-01-20'
+  }
+];
 }
--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing-module';
+import { table } from 'console';
+import { Table } from './table/table';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    SharedRoutingModule
-  ]
+    SharedRoutingModule,
+    Table
+  ],
+  exports:[Table]
 })
 export class SharedModule { }
