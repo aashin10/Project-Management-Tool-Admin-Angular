132426fa229ee93486ad2646ddeffd6c395976e4
Alan Jose <alnjoze@gmail.com>
2025-10-04 11:33:50 +0530

    fixt: fix styles

--- src/app/shared/components/search-bar/search-bar.ts
+++ src/app/shared/components/search-bar/search-bar.ts
@@ -18,11 +18,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class SearchBar {
   @Input() placeholder: string = 'Search...';
-  @Input() backgroundColor: string = 'bg-[#F7F8F9]';
+  @Input() backgroundColor: string = 'bg-[#FBFBFB]';
   @Input() height: string = 'py-2';
   @Input() width: string = 'w-full';
   @Input() border: string = 'border';
-  @Input() borderColor: string = 'border-gray-300';
+  @Input() borderColor: string = 'border-[#DFE1E6]';
 
   @Output() search = new EventEmitter<string>();
 
@@ -33,7 +33,7 @@ export class SearchBar {
   }
 
   get inputClasses(): string {
-    return `${this.width} ${this.height} ${this.border} ${this.borderColor} ${this.backgroundColor} pl-8 pr-4 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-transparent text-sm placeholder-gray-400 text-gray-800`;
+    return `${this.width} ${this.height} ${this.border} ${this.borderColor} ${this.backgroundColor} pl-8 pr-4 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-transparent text-sm placeholder-[#8993A4] text-black`;
   }
 
   onSearch(): void {
