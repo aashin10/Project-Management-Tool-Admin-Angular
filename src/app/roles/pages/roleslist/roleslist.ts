ccc15170768183376f0f9822e476b204736a5fa5
Afrin Amina <afrin.amina@experionglobal.local>
2025-10-06 09:11:07 +0530

    Updated create role modal

--- src/app/roles/pages/roleslist/roleslist.ts
+++ src/app/roles/pages/roleslist/roleslist.ts
@@ -2,23 +2,24 @@ import { Component } from '@angular/core';
 import { Sectiontitle } from '../../../shared/sectiontitle/sectiontitle';
 import { NgFor } from '@angular/common';
 import { SearchBar } from '../../../shared/components/search-bar/search-bar';
-import { table } from 'console';
 import { Table,TableColumn} from '../../../shared/table/table';
+import { RolesModal } from '../roles-modal/roles-modal';
 
-interface Role {
-  name: string;
-  description: string;
-  users: number;
-  created: string;
-}
+// interface Role {
+//   name: string;
+//   description: string;
+//   users: number;
+//   created: string;
+// }
 
 @Component({
   selector: 'app-roleslist',
-  imports: [Sectiontitle,NgFor,SearchBar,Table],
+  imports: [Sectiontitle,NgFor,SearchBar,Table,RolesModal],
   templateUrl: './roleslist.html',
   styleUrl: './roleslist.css'
 })
 export class Roleslist {
+  isModalOpen: boolean = false;
    roles = [
     { name: 'Admin', description: 'Full access to system', users: 10, created: '2024-01-05' },
     { name: 'Manager', description: 'Manage teams and projects', users: 6, created: '2024-02-12' },
@@ -47,5 +48,21 @@ export class Roleslist {
 
   handleTableAction(event: { action: string; row: any }) {
     console.log('Action clicked:', event);
+  }
+   openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  createRole(newRole: any) {
+    this.roles.push({
+      ...newRole,
+      users: 0,
+      created: new Date().toISOString().split('T')[0]
+    });
+    this.closeModal();
   }
 }
