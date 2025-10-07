3ef9f8d390ce3534fb09c8de31aa8a3483d0d389
Alan Jose <alnjoze@gmail.com>
2025-10-07 14:55:45 +0530

    fix: fixed project list not showing after authorization

--- src/app/projects/components/select-projects-section/select-projects-section.ts
+++ src/app/projects/components/select-projects-section/select-projects-section.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CustomButton } from '../../../shared/custom-button/custom-button';
 import { SearchBar } from '../../../shared/components/search-bar/search-bar';
 import { Importprojectslist } from '../importprojectslist/importprojectslist';
 import { Jiraservice } from '../../pages/importfromjira/jiraservice';
+import { CommonModule } from '@angular/common';
+import { LoadingIndicator } from '../../../shared/loading-indicator/loading-indicator';
 
 @Component({
   selector: 'app-select-projects-section',
-  imports: [CustomButton, SearchBar, Importprojectslist],
+  imports: [CustomButton, SearchBar, Importprojectslist, CommonModule, LoadingIndicator],
   templateUrl: './select-projects-section.html',
   styleUrl: './select-projects-section.css',
 })
 export class SelectProjectsSection implements OnInit {
-  public constructor(private jiraService: Jiraservice) {}
+  constructor(private jiraService: Jiraservice, private cdr: ChangeDetectorRef) {}
+
   projects: any[] = [];
-  tempProjects = [];
+  tempProjects: any[] = [];
+  loadingProjects: boolean = true;
   async ngOnInit() {
     const token = sessionStorage.getItem('jira_access_token');
     if (token) {
@@ -24,15 +28,17 @@ export class SelectProjectsSection implements OnInit {
         const importedProjects: any = await this.jiraService.fetchJiraProjects(token, cloudId);
         console.log('Jira Projects:', importedProjects);
         importedProjects.forEach((project: any) => {
-          this.projects.push({
+          this.tempProjects.push({
             name: project.name,
             key: project.key,
             id: project.id,
             selected: false,
           });
         });
-        console.log('Mapped Projects:', this.projects);
       }
     }
+    this.projects = [...this.tempProjects];
+    this.loadingProjects = false;
+    this.cdr.detectChanges();
   }
 }
