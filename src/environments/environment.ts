6d0ca314c4ba73d8c24654791f9ccf8b5e6e19df
Alan Jose <alnjoze@gmail.com>
2025-10-08 17:55:56 +0530

    feat: fixes and env variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  jiraTokenExchangeUrl: 'https://auth.atlassian.com/oauth/token',
+  jiraClientId: 'LlEIudxhc8hSon7CNwnNblzfGamJVvu2',
+  jiraClientSecret: 'ATOAtHqkou0PXZwlWeOCJXNq9oWDhAvZqY2aai98hLSFoWfWkBeJerdfHT95OCyRUh4P19C29ADE',
+  jiraGetAccessibleResourcesUrl: 'https://api.atlassian.com/oauth/token/accessible-resources',
+  jiraRedirectUri: 'http://localhost:4200/projects/importfromjira',
+};
